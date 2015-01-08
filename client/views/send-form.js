b4bb26823c28d3cb072220f64a3dabcbfdd9950f
David Mihal <eastsideski@gmail.com>
2015-01-08 21:36:46 +0000

    Send message with enter btn

--- client/views/send-form.js
+++ client/views/send-form.js
@@ -1,12 +1,21 @@
 Template.sendForm.events({
   'click .send': function(e, template){
-    var msg = template.find('.message').value;
-    Messages.insert({
-      channel: Session.get('channel'),
-      message: msg,
-      user: Meteor.userId(),
-      time: new Date()
-    });
-    template.find('.message').value = "";
+    sendMessage(template);
+  },
+  'keypress .message': function(e, template){
+    if (e.which === 13) {
+      sendMessage(template);
+    }
   }
 });
+
+var sendMessage = function(template){
+  var msg = template.find('.message').value;
+  Messages.insert({
+    channel: Session.get('channel'),
+    message: msg,
+    user: Meteor.userId(),
+    time: new Date()
+  });
+  template.find('.message').value = "";
+}
