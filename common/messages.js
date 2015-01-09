0dd872affe9c2dc4fca8104c7cc5f75e24c085c7
David Mihal <eastsideski@gmail.com>
2015-01-09 01:58:07 -0500

    Initial stuff for unread

--- common/messages.js
+++ common/messages.js
@@ -1,1 +1,17 @@
 Messages = new Mongo.Collection('messages');
+
+setRead = function(msg){
+  var collection = Collections.find(msg.channel);
+  var field = {'collections.'+msg: msg.setTimeout(function() {}, 10);};
+  Meteor.users.update(Meteor.userId(),{
+    $set: field
+  });
+};
+getUnread = function(channel){
+  var date = Meteor.user()['collections'][channel];
+  var num = Messages.find({$and: [
+    {time: {$gt: date}},
+    {channel: channel}
+    ]}).count();
+  return {date: date, num: num};
+};
